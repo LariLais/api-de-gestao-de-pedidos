a4c36555ff164c727fd0cb577f3e3e25617fd085
LariLais <larissalais0202@gmail.com>
2026-02-18 20:58:42 -0300

    feat: criação de classe AppError e middleware de tratamento de erros

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { ResponseHandler } from "../utils/responseHandler";
+import { AppError } from "../errors/appError";
+
+export function errorMiddleware(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    return ResponseHandler.error(res, err.message, err.statusCode);
+  }
+  console.log(err);
+  return ResponseHandler.error(res, "Erro interno do servidor", 500);
+}
