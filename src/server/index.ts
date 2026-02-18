70fa418a58def152205f7ad2c53a8d03cd309ab3
LariLais <larissalais0202@gmail.com>
2026-02-18 10:16:23 -0300

    feat: configuração de arquivo index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,8 @@
-const PORT = process.env.PORT ?? 3001
+import { app } from "../app/app";
 
+const PORT = process.env.PORT ?? 3001;
+const localhostUrl = `http://localhost:${PORT}`;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em ${localhostUrl}.`);
+});
